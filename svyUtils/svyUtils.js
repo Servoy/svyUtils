aa4523a0d1666ada5768e0750b4b518d468751a7
Paul Bakker <pbakker@servoy.com>
2012-09-13 08:54:19 +0000

    Added svyApplicationCore module, containing module Init logic + empty scopes for Service and Navigation managers Added svyMonitoring module, which hooks modGoogleanalytics into the frameworks

---
+++ svyUtils/svyUtils.js
@@ -0,0 +1,112 @@
+/**
+ * Returns all JSForms that are instances of a certain JSForm
+ *
+ * @param {JSForm} superForm
+ *
+ * @return {Array<JSForm>}
+ *
+ * @properties={typeid:24,uuid:"4E5567AB-4BE9-406F-ABDF-5807EF930E09"}
+ */
+function getJSFormInstances(superForm) {
+	/**@type {Array<JSForm>}*/
+	var retval = []
+	var smForms = solutionModel.getForms()
+	var smForm, instances
+	for (var i = 0; i < smForms.length; i++) {
+		smForm = smForms[i]
+		instances = []
+		if (retval.indexOf(smForm) != -1) continue //FIXME: this check doesn't work due to SVY-2711
+		while (smForm.extendsForm != null) {
+			instances.push(smForm)
+			if (smForm.extendsForm.name == superForm.name || retval.indexOf(smForm.extendsForm) != -1) { //FIXME: first clause should compare object, not name and second clause doesn't work due to SVY-2711
+				retval = retval.concat(instances)
+				break;
+			}
+			smForm = smForm.extendsForm
+		}
+	}
+	return retval
+}
+
+/**
+ * Tries to connect to the provided hostname. If connection is successful, true is returned, otherwise false. (time)
+ * @private
+ * @param {String} hostname
+ * @param {Number} [timeout] timeout for the connection check (in milliseconds). Default: 200ms
+ *
+ * @properties={typeid:24,uuid:"E307CC91-7CAA-4618-8CE8-20CFA72C08BB"}
+ */
+function testHostName(hostname, timeout) {
+	var timeOut = timeout || 200
+	var socket
+	var reachable = false;
+	try {
+		var addr = java.net.InetAddress.getByName(hostname);
+		var port = 80;
+		//Setting type to super class to prevent warnings
+		/**@type {Packages.java.net.SocketAddress}*/ 
+		var sockaddr = new java.net.InetSocketAddress(addr, port);
+
+		// Create an unbound socket
+		socket = new java.net.Socket();
+
+		// If the timeout occurs, SocketTimeoutException is thrown.
+		socket.connect(sockaddr, timeOut);
+		reachable = true;
+	} catch (e if e.javaException instanceof java.net.UnknownHostException) {
+		application.output('Host "' + hostname + '" cannot be reached, might not exist', LOGGINGLEVEL.DEBUG)
+	} catch (e if e.javaException instanceof java.net.SocketTimeoutException) {
+		application.output('Timeout checking host "' + hostname + '"', LOGGINGLEVEL.DEBUG)
+	} catch (e if e.javaException instanceof java.io.IOException) {
+		application.output('Network issue checking host "' + hostname + '"', LOGGINGLEVEL.DEBUG)
+	} catch (e) {
+		application.output(e)
+	} finally {
+		if (socket != null) {
+			try {
+				socket.close()
+			} catch (e) {
+			}
+		}
+	}
+
+	if (reachable) {
+		application.output('Host ' + hostname + ' exists', LOGGINGLEVEL.DEBUG)
+		return true
+	}
+	return false
+}
+
+/**
+ * Function to parse urls and retrieve the different parts of it. Taken from http://blog.stevenlevithan.com/archives/parseuri
+ * 
+ * @param {String} url
+ * @param {Boolean} [strictMode] Default false
+ * @return {{anchor: String, query: String, file: String, directory: String, path: String, relative: String, port: Number, host: String, password:String, user: String, userInfo: String, authority: String, protocol:String, source: String, queryKey: Object<String>}}
+ *
+ * @properties={typeid:24,uuid:"9E13D36E-F214-4B44-A13A-644256B077D3"}
+ */
+function parseUrl(url, strictMode) {
+	// parseUri 1.2.2
+	// (c) Steven Levithan <stevenlevithan.com>
+	// MIT License
+	var o = {}
+	o.strictMode = strictMode;
+	o.key = ["source", "protocol", "authority", "userInfo", "user", "password", "host", "port", "relative", "path", "directory", "file", "query", "anchor"];
+	o.q = {};
+	o.q.name = "queryKey";
+	o.q.parser = /(?:^|&)([^&=]*)=?([^&]*)/g
+	o.parser = { };
+	o.parser.strict = /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/
+	o.parser.loose = /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+
+	var m = o.parser[o.strictMode ? "strict" : "loose"].exec(url);
+	var uri = {};
+	var i = 14;
+	while (i--) uri[o.key[i]] = m[i] || "";
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function($0, $1, $2) {
+			if ($1) uri[o.q.name][$1] = $2;
+		});
+	return uri;
+}
