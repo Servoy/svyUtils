f2dccacc132ec00dd33120997e9a1a80037c53e5
Patrick Ruhsert <pruhsert@servoy.com>
2012-09-23 12:30:14 +0000

    added svyDateUtils with helper methods for many common Date operations

---
+++ svyUtils/svyDateUtils.js
@@ -0,0 +1,739 @@
+/*
+ * Date helper methods
+ * 
+ */
+
+/**
+ * A java.util.Calendar instance used to do the math
+ * @private
+ * @properties={typeid:35,uuid:"B31EC585-12D8-4E8E-9449-788A96A22C6F",variableType:-4}
+ */
+var calendar = java.util.Calendar.getInstance();
+
+/**
+ * Adds the value of the given field to the given date
+ * 
+ * @private 
+ * 
+ * @param {Date} date
+ * @param {Number} dateField
+ * @param {Number} value
+ * 
+ * @return {Date}
+ *
+ * @properties={typeid:24,uuid:"C2BBF1A3-1D58-44F7-8E50-50D0C7510BD5"}
+ */
+function addToDate(date, dateField, value) {
+	calendar.setTimeInMillis(date.getTime());
+	calendar.add(dateField, value);
+	return new Date(calendar.getTimeInMillis());
+}
+
+/**
+ * Adds the number of years, months, days, hours, minutes and seconds to the given date and returns a new date
+ * 
+ * @param {Date} date
+ * @param {Number} years
+ * @param {Number} months
+ * @param {Number} days
+ * @param {Number} hours
+ * @param {Number} minutes
+ * @param {Number} seconds
+ * 
+ * @return {Date} newDate
+ *
+ * @properties={typeid:24,uuid:"0FAD9785-1C41-424D-B459-B5A116055BE8"}
+ */
+function add(date, years, months, days, hours, minutes, seconds) {
+	calendar.setTimeInMillis(date.getTime());	
+	calendar.add(java.util.Calendar.YEAR, years);
+	calendar.add(java.util.Calendar.MONTH, months);
+	calendar.add(java.util.Calendar.DATE, days);
+	calendar.add(java.util.Calendar.HOUR, hours);
+	calendar.add(java.util.Calendar.MINUTE, minutes);
+	calendar.add(java.util.Calendar.SECOND, seconds);
+	return new Date(calendar.getTimeInMillis());	
+}
+
+/**
+ * Adds the given number of years to the given date and returns a new date<br>
+ * Negative number of years will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} years - number of years to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"645D8BC5-41F0-48C9-879D-FD3B30BFF200"}
+ */
+function addYears(date, years) {
+	return addToDate(date, java.util.Calendar.YEAR, years);
+}
+
+/**
+ * Adds the given number of months to the given date and returns a new dat<br>
+ * Negative number of months will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} months - number of months to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"A7351ED8-23C6-47BE-A0F8-AFE239DC027F"}
+ */
+function addMonths(date, months) {
+	return addToDate(date, java.util.Calendar.MONTH, months);
+}
+
+/**
+ * Adds the given number of weeks to the given date and returns a new dat<br>
+ * Negative number of weeks will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} weeks - number of weeks to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"9F329C32-10B8-4899-AD26-A29A6E4DF898"}
+ */
+function addWeeks(date, weeks) {
+	return addToDate(date, java.util.Calendar.WEEK_OF_YEAR, weeks);
+}
+
+/**
+ * Adds the given number of days to the given date and returns a new dat<br>
+ * Negative number of days will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} days - number of days to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"1A18C63D-8BF3-4821-A785-89976D09B0E1"}
+ */
+function addDays(date, days) {
+	return addToDate(date, java.util.Calendar.DATE, days);
+}
+
+/**
+ * Adds the given number of hours to the given date and returns a new dat<br>
+ * Negative number of hours will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} hours - number of hours to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"58E508CF-F61D-4818-A539-04EB5B6B2D12"}
+ */
+function addHours(date, hours) {
+	return addToDate(date, java.util.Calendar.HOUR, hours);
+}
+
+/**
+ * Adds the given number of minutes to the given date and returns a new dat<br>
+ * Negative number of minutes will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} minutes - number of minutes to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"2463F39B-FE86-4A04-A147-2373231BA93E"}
+ */
+function addMinutes(date, minutes) {
+	return addToDate(date, java.util.Calendar.MINUTE, minutes);
+}
+
+/**
+ * Adds the given number of seconds to the given date and returns a new dat<br>
+ * Negative number of seconds will be substracted
+ * 
+ * @param {Date} date - the date to add to
+ * @param {Number} seconds - number of seconds to add
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"53B59AE5-3788-44A3-9ABA-2EB006F33C2B"}
+ */
+function addSeconds(date, seconds) {
+	return addToDate(date, java.util.Calendar.SECOND, seconds);
+}
+
+/**
+ * Sets the time of the given date to 00:00:00.000
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"C9F1565E-8992-4166-A897-F0CF3E9807D4"}
+ */
+function toStartOfDay(date) {
+	date.setHours(0, 0, 0, 0);
+	return date;
+}
+
+/**
+ * Sets the time of the given date to 00:00:00
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} result
+ *
+ * @properties={typeid:24,uuid:"A5174FF5-102D-4C85-ABE0-C12C62E59C16"}
+ */
+function toEndOfDay(date) {
+	date.setHours(23, 59, 59, 999);
+	return date;
+}
+
+/**
+ * Creates a from - to search String for the two dates<br>
+ * 
+ * The range starts at the given start date at 00:00:00 and<br>
+ * ends at the given end date at 23:59:59<br>
+ * 
+ * @see createDateTimeSearchString(start,end) if exact datetime search is needed
+ * 
+ * @param {Date} start
+ * @param {Date} end
+ * 
+ * @return {String} searchString
+ *
+ * @properties={typeid:24,uuid:"5AAA29BB-51F7-4D71-8213-E6885D7FFD2C"}
+ */
+function createDateSearchString(start, end) {
+	start.setHours(0, 0, 0, 0);
+	end.setHours(23, 59, 59, 999);
+	var pattern = "yyyy-MM-dd HH:mm:ss";
+	var fromString = utils.dateFormat(start, pattern);
+	var toString = utils.dateFormat(end, pattern);
+	return fromString + "..." + toString + "|" + pattern;
+}
+
+/**
+ * Creates a from - to search String for the two dates<br>
+ * 
+ * @see createDateSearchString(start, end) if date only search is needed
+ * 
+ * @param {Date} start
+ * @param {Date} end
+ * 
+ * @return {String} searchString
+ *
+ * @properties={typeid:24,uuid:"9A1F4995-0DC8-4153-BDD9-A4CDFDAAF327"}
+ */
+function createDateTimeSearchString(start, end) {
+	var pattern = "yyyy-MM-dd HH:mm:ss";
+	var fromString = utils.dateFormat(start, pattern);
+	var toString = utils.dateFormat(end, pattern);
+	return fromString + "..." + toString + "|" + pattern;
+}
+
+/**
+ * Returns the number of full days between the two dates
+ * 
+ * @param {Date} start
+ * @param {Date} end
+ * 
+ * @return {Number} fullDaysBetween
+ *
+ * @properties={typeid:24,uuid:"108B647E-FC66-4843-8E83-0ACAA9A68720"}
+ */
+function getDayDifference(start, end) {
+	return Math.ceil(((end.getTime() - start.getTime()) / 86400000));
+}
+
+/**
+ * Returns an array containing the names of the months for either the current or the given Locale
+ * 
+ * @param {String} [locale] - the optional Locale
+ * 
+ * @return {String[]} monthNames
+ *
+ * @properties={typeid:24,uuid:"FA8FCF0C-7D26-484B-977D-95D290F4753A"}
+ */
+function getMonthNames(locale) {
+	var dfs;
+	if (locale) {
+		var l = new java.util.Locale(locale);
+		dfs = new java.text.DateFormatSymbols(l);		
+	} else {
+		dfs = new java.text.DateFormatSymbols();
+	}
+	return dfs.getMonths();
+}
+
+/**
+ * Returns an array containing the short names of the months for either the current or the given Locale
+ * 
+ * @param {String} [locale] - the optional Locale
+ * 
+ * @return {String[]} shortMonthNames
+ *
+ * @properties={typeid:24,uuid:"6D7CAA25-0203-41E0-8F2E-68996BE73CF3"}
+ */
+function getShortMonthNames(locale) {
+	var dfs;
+	if (locale) {
+		var l = new java.util.Locale(locale);
+		dfs = new java.text.DateFormatSymbols(l);		
+	} else {
+		dfs = new java.text.DateFormatSymbols();
+	}
+	return dfs.getShortMonths();
+}
+
+/**
+ * Returns an array containing the names of the weekdays for either the current or the given Locale
+ * 
+ * @param {String} [locale] - the optional Locale
+ * 
+ * @return {String[]} weekdayNames
+ * 
+ * @example // returns an array of all week days in French<br>
+ * var dayNames = scopes.svyDateUtils.getWeekdayNames("fr");
+ *
+ * @properties={typeid:24,uuid:"69A5E277-70E9-4E4E-97BA-994876865767"}
+ */
+function getWeekdayNames(locale) {
+	var dfs;
+	if (locale) {
+		var l = new java.util.Locale(locale);
+		dfs = new java.text.DateFormatSymbols(l);		
+	} else {
+		dfs = new java.text.DateFormatSymbols();
+	}
+	return dfs.getWeekdays();
+}
+
+/**
+ * Returns an array containing the short names of the weekdays for either the current or the given Locale
+ * 
+ * @param {String} [locale] - the optional Locale
+ * 
+ * @return {String[]} shortWeekdayNames
+ * 
+ * @example // returns an array of all the short names of the week days in French<br>
+ * var dayNames = scopes.svyDateUtils.getShortWeekdayNames("fr");
+ *
+ * @properties={typeid:24,uuid:"9A35983C-A6CC-4139-B9C9-2F55380FE747"}
+ */
+function getShortWeekdayNames(locale) {
+	var dfs;
+	if (locale) {
+		var l = new java.util.Locale(locale);
+		dfs = new java.text.DateFormatSymbols(l);		
+	} else {
+		dfs = new java.text.DateFormatSymbols();
+	}
+	return dfs.getWeekdays();
+}
+
+/**
+ * Returns true if the given year is a leap year
+ * 
+ * @param {Number} year
+ * 
+ * @return {Boolean} isLeapYear
+ *
+ * @properties={typeid:24,uuid:"E74C21C0-0118-47C3-8B0F-532F95E269AA"}
+ */
+function isLeapYear(year) {
+	var cal = new java.util.GregorianCalendar();
+    return cal.isLeapYear(year);
+}
+
+/**
+ * Returns the day of the week
+ * 
+ * @param {Date} date
+ *
+ * @return {Number} dayOfWeek
+ * 
+ * @properties={typeid:24,uuid:"EAB40554-2809-4581-8A41-14EFE74F4B32"}
+ */
+function getDayOfWeek(date) {
+	calendar.setTimeInMillis(date.getTime());
+	return calendar.get(java.util.Calendar.DAY_OF_WEEK);
+}
+
+/**
+ * Returns the day of the year
+ * 
+ * @param {Date} date
+ *
+ * @return {Number} dayOfYear
+ * 
+ * @properties={typeid:24,uuid:"0EA61AF3-2829-4E42-A88C-FFB6BC23896F"}
+ */
+function getDayOfYear(date) {
+	calendar.setTimeInMillis(date.getTime());
+	return calendar.get(java.util.Calendar.DAY_OF_WEEK);
+}
+
+/**
+ * Returns a new date of the first day of the week of the given date
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} firstDayOfWeek
+ *
+ * @properties={typeid:24,uuid:"0A1D71F3-9BDB-4C34-A8CF-CC5636C106F0"}
+ */
+function getFirstDayOfWeek(date) {
+	calendar.setTimeInMillis(date.getTime());
+	var tmp = java.util.Calendar.getInstance();
+	tmp.clear();
+	tmp.set(java.util.Calendar.YEAR, calendar.get(java.util.Calendar.YEAR));
+	tmp.set(java.util.Calendar.MONTH, calendar.get(java.util.Calendar.MONTH));
+	tmp.set(java.util.Calendar.WEEK_OF_MONTH, calendar.get(java.util.Calendar.WEEK_OF_MONTH));
+	return new Date(tmp.getTimeInMillis());
+}
+
+/**
+ * Returns a new date of the first day of the month of the given date
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} firstDayOfMonth
+ *
+ * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+ */
+function getFirstDayOfMonth(date) {
+	return new Date(date.getFullYear(), date.getMonth(), 1);
+}
+
+/**
+ * Returns a new date of the first day of the month of the given date
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} firstDayOfMonth
+ *
+ * @properties={typeid:24,uuid:"93B1C8A8-4F25-46C9-95E7-DE970B669115"}
+ */
+function getFirstDayOfYear(date) {
+	return new Date(date.getFullYear(), 0, 1);
+}
+
+/**
+ * Returns a new date of the last day of the week of the given date
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} lastDayOfWeek
+ *
+ * @properties={typeid:24,uuid:"89AE5866-6633-4E5E-8001-E985E0B29A57"}
+ */
+function getLastDayOfWeek(date) {
+	calendar.setTimeInMillis(date.getTime());
+	var tmp = java.util.Calendar.getInstance();
+	tmp.clear();
+	tmp.set(java.util.Calendar.YEAR,calendar.get(java.util.Calendar.YEAR));
+	tmp.set(java.util.Calendar.WEEK_OF_YEAR, calendar.get(java.util.Calendar.WEEK_OF_YEAR));
+	tmp.add(java.util.Calendar.DAY_OF_MONTH, 6);
+	return new Date(tmp.getTimeInMillis());
+}
+
+/**
+ * Returns a new date of the last day of the year of the given date
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} lastDayOfYear
+ *
+ * @properties={typeid:24,uuid:"3C204742-6D57-47F9-AEB8-6C0712765F6D"}
+ */
+function getLastDayOfYear(date) {
+	calendar.setTimeInMillis(date.getTime());
+	var tmp = java.util.Calendar.getInstance();
+	tmp.clear();
+	tmp.set(java.util.Calendar.YEAR,calendar.get(java.util.Calendar.YEAR) + 1);
+	tmp.add(java.util.Calendar.DAY_OF_YEAR, -1);
+	return new Date(tmp.getTimeInMillis());
+}
+
+/**
+ * Returns a ISO 8601 formatted String of the given Date
+ * 
+ * @param {Date} date
+ *
+ * @properties={typeid:24,uuid:"AB969B10-57FA-498E-9E05-7311D678D18F"}
+ */
+function getISODateTime(date) {
+	return utils.dateFormat(date,"yyyyMMdd'T'HHmmss");
+}
+
+/**
+ * Returns a ISO 8601 date only formatted String of the given Date
+ * 
+ * @param {Date} date
+ *
+ * @properties={typeid:24,uuid:"A3DC35D1-E862-476E-83B4-467EFFD37FC3"}
+ */
+function getISODate(date) {
+	return utils.dateFormat(date,"yyyyMMdd");
+}
+
+/**
+ * Returns a ISO 8601 time only formatted String of the given Date
+ * 
+ * @param {Date} date
+ *
+ * @properties={typeid:24,uuid:"62F0C2E9-C492-48E7-AA5B-3FD589DE2AE8"}
+ */
+function getISOTime(date) {
+	return utils.dateFormat(date,"'T'HHmmss");
+}
+
+/**
+ * Returns a new date of the last day of the month of the given date
+ * 
+ * @param {Date} date
+ * 
+ * @return {Date} lastDayOfMonth
+ *
+ * @properties={typeid:24,uuid:"3FA2E1A8-7640-4DBB-9A0F-2C4B63C0B3F9"}
+ */
+function getLastDayOfMonth(date) {
+	calendar.setTimeInMillis(date.getTime());
+	var tmp = java.util.Calendar.getInstance();
+	tmp.clear();
+	tmp.set(java.util.Calendar.YEAR, calendar.get(java.util.Calendar.YEAR));
+	tmp.set(java.util.Calendar.MONTH, calendar.get(java.util.Calendar.MONTH) + 1);
+	tmp.add(java.util.Calendar.DATE, -1);
+	return new Date(tmp.getTimeInMillis());
+}
+
+/**
+ * Takes the time of the given date and returns the hours as a decimal value
+ * 
+ * @param {Date} date
+ *
+ * @properties={typeid:24,uuid:"F4732B65-BF91-4EAF-9925-F8E11988FB7D"}
+ */
+function getDecimalHours(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes() / 60 + date.getSeconds() / 3600 + date.getMilliseconds() / 3600000;
+	return hours + minutes;
+}
+
+/**
+ * Creates a DateTime object that can be used to chain methods of this class<br>
+ * as for example <code>dateTimeObject.addDays(5).toStartOfDay().date</code>
+ * 
+ * @constructor 
+ * @param {Date} date
+ *
+ * @properties={typeid:24,uuid:"9651D079-AD0D-4769-A766-FED23DD2ED99"}
+ */
+function DateTime(date) {
+	
+	/**
+	 * The Date object of this DateTime
+	 */
+	this.date = date;
+	
+	/**
+	 * Adds the given number of days to the given date<br>
+	 * Negative number of days will be substracted
+	 *
+	 * @param {Number} days - number of days to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addDays = function(days) {
+		this.date = addDays(this.date, days);
+		return this;
+	}
+	
+	/**
+	 * Adds the given number of hours to the given date<br>
+	 * Negative number of hours will be substracted
+	 *
+	 * @param {Number} hours - number of hours to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addHours = function(hours) {
+		this.date = addHours(this.date,hours);
+		return this;
+	}
+	
+	/**
+	 * Adds the given number of minutes to the given date<br>
+	 * Negative number of minutes will be substracted
+	 *
+	 * @param {Number} minutes - number of minutes to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addMinutes = function(minutes) {
+		this.date = addMinutes(this.date,minutes);
+		return this;
+	}
+	
+	/**
+	 * Adds the given number of months to the given date<br>
+	 * Negative number of months will be substracted
+	 *
+	 * @param {Number} months - number of months to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addMonths = function(months) {
+		this.date = addMonths(this.date,months);
+		return this;
+	}
+	
+	/**
+	 * Adds the given number of seconds to the given date<br>
+	 * Negative number of seconds will be substracted
+	 *
+	 * @param {Number} seconds - number of seconds to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addSeconds = function(seconds) {
+		this.date = addSeconds(this.date,seconds);
+		return this;
+	}
+	
+	/**
+	 * Adds the given number of weeks to the given date<br>
+	 * Negative number of weeks will be substracted
+	 *
+	 * @param {Number} weeks - number of weeks to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addWeeks = function(weeks) {
+		this.date = addWeeks(this.date,weeks);
+		return this;
+	}	
+	
+	/**
+	 * Adds the given number of years to the given date<br>
+	 * Negative number of years will be substracted
+	 *
+	 * @param {Number} years - number of years to add
+	 *
+	 * @return {DateTime}
+	 */
+	this.addYears = function(years) {
+		this.date = addYears(this.date,years);
+		return this;
+	}
+	
+	/**
+	 * Returns the day of the week
+	 *
+	 * @return {Number} dayOfWeek
+	 */
+	this.getDayOfWeek = function() {
+		return getDayOfWeek(this.date);
+	}
+	
+	/**
+	 * Returns the day of the year
+	 *
+	 * @return {Number} dayOfYear
+	 */
+	this.getDayOfYear = function() {
+		return getDayOfYear(this.date);
+	}	
+	
+	/**
+	 * Sets the time of the given date to 00:00:00.000
+	 * 
+	 * @return {DateTime}
+	 */
+	this.toStartOfDay = function() {
+		this.date = toStartOfDay(this.date);
+		return this;
+	}
+	
+	/**
+	 * Sets the time of the given date to 23:59:59.999
+	 * 
+	 * @return {DateTime}
+	 */
+	this.toEndOfDay = function() {
+		this.date = toEndOfDay(this.date);
+		return this;
+	}	
+	
+	/**
+	 * Sets this DateTime to the first day of the month
+	 * 
+	 * @return {DateTime}
+	 *
+	 * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+	 */
+	this.toFirstDayOfMonth = function() {
+		this.date = getFirstDayOfMonth(this.date);
+		return this;
+	}
+	
+	/**
+	 * Sets this DateTime to the first day of the week
+	 * 
+	 * @return {DateTime}
+	 *
+	 * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+	 */
+	this.toFirstDayOfWeek = function() {
+		this.date = getFirstDayOfWeek(this.date);
+		return this;
+	}	
+	
+	/**
+	 * Sets this DateTime to the first day of the year
+	 * 
+	 * @return {DateTime}
+	 *
+	 * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+	 */
+	this.toFirstDayOfYear = function() {
+		this.date = getFirstDayOfWeek(this.date);
+		return this;
+	}	
+	
+	/**
+	 * Sets this DateTime to the first day of the month
+	 * 
+	 * @return {DateTime}
+	 *
+	 * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+	 */
+	this.toLastDayOfMonth = function() {
+		this.date = getLastDayOfMonth(this.date);
+		return this;
+	}
+	
+	/**
+	 * Sets this DateTime to the last day of the week
+	 * 
+	 * @return {DateTime}
+	 *
+	 * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+	 */
+	this.toLastDayOfWeek = function() {
+		this.date = getLastDayOfWeek(this.date);
+		return this;
+	}	
+	
+	/**
+	 * Sets this DateTime to the last day of the year
+	 * 
+	 * @return {DateTime}
+	 *
+	 * @properties={typeid:24,uuid:"7C02ADA9-3E54-4D48-9A36-81D228CDD802"}
+	 */
+	this.toLastDayOfYear = function() {
+		this.date = getLastDayOfWeek(this.date);
+		return this;
+	}		
+}
