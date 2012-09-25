e03ba2531dd841eee775b347a8c413e556425a04
Sean Devlin <sdevlin@servoy.com>
2012-09-25 14:36:47 +0000

    Added new scope to utils 'svyUI' for all things UI related: Including 2 methods for handling changes on checkbox value lists - getCheckBoxValueListItemAdded - getCheckBoxValueListItemRemoved
    Determines the item that was added or removed from a checkbox valuelist

---
+++ svyUtils/svyUI.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Object} oldValue
+ * @param {Object} newValue
+ * @return {Object} addedItem
+ * @properties={typeid:24,uuid:"42F033EE-939F-4E4B-BFE8-02DF8B31B677"}
+ */
+function getCheckBoxValueListItemAdded(oldValue,newValue){
+	var oldItems = (oldValue) ? oldValue.toString().split('\n') : [];
+	var newItems = (newValue) ? newValue.toString().split('\n') : [];
+	if(newItems.length <= oldItems.length) return null;
+	newItems.sort();
+	oldItems.sort();
+    for(i in oldItems){
+        if(oldItems[i] != newItems[i])
+        	return newItems[i];
+    }
+    return newItems[newItems.length - 1];
+}
+
+/**
+ * @param {Object} oldValue
+ * @param {Object} newValue
+ * @return {Object} addedItem
+ * @properties={typeid:24,uuid:"F9B3D03A-1738-4B88-B7D8-F925504A7453"}
+ */
+function getCheckBoxValueListItemRemoved(oldValue,newValue){
+	var oldItems = (oldValue) ? oldValue.toString().split('\n') : [];
+	var newItems = (newValue) ? newValue.toString().split('\n') : [];
+	if(newItems.length >= oldItems.length) return null;
+	newItems.sort();
+	oldItems.sort();
+    for(i in newItems){
+        if(newItems[i] != oldItems[i])
+        	return oldItems[i];
+    }
+    return oldItems[oldItems.length - 1];
+}
