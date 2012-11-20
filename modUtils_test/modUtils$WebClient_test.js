063c0ed65bac3e5f6a785ee8e0572ffd82834fe0
Paul Bakker <pbakker@servoy.com>
2012-11-20 19:49:33 +0000

    First round of refactoring, moving from svyUtils > modUtils - created new modules - moved scopes to new modules - updated module dependancies to modUtils($.....)

---
+++ modUtils_test/modUtils$WebClient_test.js
@@ -0,0 +1,11 @@
+/**
+ * @properties={typeid:24,uuid:"1A330EA0-A1A5-4179-B12B-24060448E6D4"}
+ */
+function testJavaDependencies() {
+	jsunit.assertEquals('Failed to instatiate Packages.org.apache.wicket.behavior.SimpleAttributeModifier', 'function', typeof Packages.org.apache.wicket.behavior.SimpleAttributeModifier)
+	jsunit.assertEquals('Failed to instatiate Packages.org.apache.wicket.behavior.HeaderContributor', 'function', typeof Packages.org.apache.wicket.behavior.HeaderContributor)
+	jsunit.assertEquals('Failed to instatiate Packages.org.apache.wicket.markup.html.IHeaderContributor', 'function', typeof Packages.org.apache.wicket.markup.html.IHeaderContributor)
+	jsunit.assertEquals('Failed to instatiate Packages.org.apache.wicket.ResourceReference', 'function', typeof Packages.org.apache.wicket.ResourceReference)
+	jsunit.assertEquals('Failed to instatiate Packages.org.apache.wicket.RequestCycle', 'function', typeof Packages.org.apache.wicket.RequestCycle)
+	jsunit.assertEquals('Failed to instatiate Packages.java.util.ArrayList', 'function', typeof Packages.java.util.ArrayList)
+}
