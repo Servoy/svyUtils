f44c8353fbb617445cce84eebe7da05602e9fdbe
Paul Bakker <pbakker@servoy.com>
2012-11-28 08:26:32 +0000

    Added test scope for svyExceptions

---
+++ modUtils_test/svyExceptions_test.js
@@ -0,0 +1,18 @@
+/**
+ * @properties={typeid:24,uuid:"9A19D2B3-0124-4C45-9A13-F8ADC7982B70"}
+ */
+function testExceptionHierarchy() {
+	function TestException(message) {
+		scopes.svyExceptions.SvyException.call(this, message);
+	}
+
+	TestException.prototype = new scopes.svyExceptions.SvyException();
+
+	var x = new TestException('testing')
+	
+	jsunit.assertTrue(x instanceof TestException)
+	jsunit.assertTrue(x instanceof scopes.svyExceptions.SvyException)
+	
+	
+	jsunit.assertEquals('testing',x.message)
+}
