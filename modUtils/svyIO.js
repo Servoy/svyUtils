763942670c158f51ec1975dac4ef68e37732366f
Paul Bakker <pbakker@servoy.com>
2012-12-04 15:36:07 +0000

    Added svyIO and svySystem scopes Deprecated old implementations now available in svyIO and svySystem Deprecated unused and bogus methods Added several TODO's

---
+++ modUtils/svyIO.js
@@ -0,0 +1,221 @@
+/**
+ * Unzips the given file to the given target file<p>
+ * 
+ * If no target file is given, all files in fileToUnzip will<br>
+ * be extracted to a directory with the same name as the zip file
+ * 
+ * @param {plugins.file.JSFile} fileToUnzip
+ * @param {plugins.file.JSFile} [targetFile]
+ * 
+ * @return {plugins.file.JSFile} targetFile
+ * 
+ * @author patrick
+ * @since 2012-10-15
+ * 
+ * @properties={typeid:24,uuid:"757E9367-14BB-4488-883E-BD79E6531B0B"}
+ */
+function unzip(fileToUnzip, targetFile) {
+	var zipFilePath = fileToUnzip.getAbsolutePath();
+	var fileSeparator = java.io.File.separator;
+	if (!targetFile) {
+		targetFile = plugins.file.convertToJSFile(zipFilePath.substr(0, zipFilePath.lastIndexOf(".")));
+		if (targetFile.exists()) {
+			do {
+				targetFile = plugins.file.convertToJSFile(targetFile.getAbsolutePath() + "-1");
+			} while (targetFile.exists());
+		}
+	}
+	
+	if (!targetFile.exists()) {
+		targetFile.mkdirs();
+	}
+	
+	try {
+		var zip = new java.util.zip.ZipFile(fileToUnzip.getAbsolutePath());
+		var zipEntries = zip.entries();
+		
+		while (zipEntries.hasMoreElements()) {
+			/** @type {java.util.zip.ZipEntry} */
+			var zipEntry = zipEntries.nextElement();
+			var zipEntryName = zipEntry.getName();
+			zipEntryName = utils.stringReplace(zipEntryName, "/", java.io.File.separator);
+			if (zipEntry.isDirectory()) {
+				var zipDir = plugins.file.convertToJSFile(targetFile.getAbsolutePath() + fileSeparator + zipEntryName);
+				zipDir.mkdirs();
+				continue;
+			} else {
+				var zipFile = plugins.file.convertToJSFile(targetFile.getAbsolutePath() + fileSeparator + zipEntryName);
+				if (!zipFile.getParentFile().exists()) {
+					zipFile.getParentFile().mkdirs();
+				}
+				var is = zip.getInputStream(zipEntry);
+				/** @type {java.io.OutputStream} */
+				var fos = new java.io.FileOutputStream(zipFile.getAbsolutePath());
+				
+				/** @type {java.nio.channels.ReadableByteChannel} */
+				var inputChannel = java.nio.channels.Channels.newChannel(is);
+				/** @type {java.nio.channels.WritableByteChannel} */
+				var outputChannel = java.nio.channels.Channels.newChannel(fos);		
+
+				channelCopy(inputChannel, outputChannel);
+
+				is.close();
+				outputChannel.close();
+				fos.close();
+			}
+		}
+	} catch (e) {
+		// IO Exception
+		application.output("Failed to unzip file \"" + fileToUnzip.getAbsolutePath() + "\": " + e.message);
+		return null;
+	}
+	
+	return targetFile;
+}
+
+/**
+ * Zips the given file or directory<p>
+ * 
+ * The zip file will either be written to the given target file
+ * or a zip file is created using the same name and location as the original file<p>
+ * 
+ * Note: if the target file already exists, it will be <b>deleted</b>
+ * 
+ * @param {plugins.file.JSFile} fileToZip
+ * @param {plugins.file.JSFile} [targetFile]
+ * @param {Array<String>} [filenamesToStoreUncompressed] array of file names that should be stored uncompressed in the archive
+ * 
+ * @return {plugins.file.JSFile} zipFile
+ * 
+ * @throws {Error}
+ * 
+ * @author patrick
+ * @since 2012-10-15
+ *
+ * @properties={typeid:24,uuid:"7483C635-ACD5-42D9-9109-9B97DDE9E5AD"}
+ */
+function zip(fileToZip, targetFile, filenamesToStoreUncompressed) {
+	var filePath = fileToZip.getAbsolutePath();
+	if (!targetFile) {
+		targetFile = plugins.file.convertToJSFile(filePath + ".zip");
+	}
+	
+	if (targetFile.exists()) {
+		if (!targetFile.deleteFile()) {
+			return null;
+		}
+	}
+	
+	try {
+		/** @type {java.util.zip.ZipOutputStream} */
+		var zos = new java.util.zip.ZipOutputStream(new java.io.FileOutputStream(targetFile.getAbsolutePath()));
+		
+		/** @type {java.nio.channels.WritableByteChannel} */
+		var outputChannel = java.nio.channels.Channels.newChannel(zos);		
+		
+		/**
+		 * @param {plugins.file.JSFile} file
+		 * @param {plugins.file.JSFile} base
+		 * @param {java.util.zip.ZipOutputStream} zipOutputStream
+		 */
+		function zipFile(file, base, zipOutputStream) {
+			if (file.isDirectory()) {
+				var files = file.listFiles();
+				for (var i = 0; i < files.length; i++) {
+					var singleFile = files[i];
+					zipFile(singleFile, base, zipOutputStream);
+				}
+				if (!files || files.length == 0) {
+					// empty directory
+					zipOutputStream.putNextEntry(new java.util.zip.ZipEntry(file.getPath().substring(base.getPath().length + 1) + "/"));
+				}
+			} else {
+				/** @type {java.io.InputStream} */
+				var is = new java.io.FileInputStream(file);
+				var entryPath;
+				if (file.getAbsolutePath() == base.getAbsolutePath()) {
+					entryPath = file.getName();
+				} else {
+					entryPath = file.getPath().substring(base.getPath().length + 1);
+				}
+				entryPath = utils.stringReplace(entryPath, java.io.File.separator, "/");
+				var entry = new java.util.zip.ZipEntry(entryPath);
+				
+				if (filenamesToStoreUncompressed && filenamesToStoreUncompressed.indexOf(file.getName()) != -1) {
+					entry.setMethod(java.util.zip.ZipEntry.STORED);
+					entry.setSize(file.size());
+					var crc = new java.util.zip.CRC32();
+					crc.update(file.getBytes());
+					entry.setCrc(crc.getValue());
+				}
+				
+				zipOutputStream.putNextEntry(entry);
+				
+				/** @type {java.nio.channels.ReadableByteChannel} */
+				var inputChannel = java.nio.channels.Channels.newChannel(is);
+				
+				channelCopy(inputChannel, outputChannel);
+				
+				is.close();
+			}
+		}
+			
+		zipFile(fileToZip, fileToZip, zos);
+		
+		outputChannel.close();
+		outputChannel = null;
+		
+		zos.close();
+		zos = null;
+	}
+	catch(e) {
+		application.output("Error zipping file \"" + fileToZip.getAbsolutePath() + "\": " + e, LOGGINGLEVEL.ERROR);
+		throw e;
+	} finally {
+		try {
+			if (outputChannel != null) {
+				outputChannel.close();
+			}
+			if (zos != null) {
+				zos.close();
+			}
+		} catch(e) {
+		}
+	}
+	
+	return targetFile;
+}
+
+/**
+ * Copies streams
+ * 
+ * @param {java.nio.channels.ReadableByteChannel} src
+ * @param {java.nio.channels.WritableByteChannel} dest
+ * 
+ * @private 
+ * 
+ * @author patrick
+ * @since 2012-10-15
+ *
+ * @properties={typeid:24,uuid:"C59BD222-0EAF-4A35-8778-BA8D3B18B985"}
+ */
+function channelCopy(src, dest) {
+	var buffer = java.nio.ByteBuffer.allocateDirect(16 * 1024);
+	while (src.read(buffer) != -1) {
+		// prepare the buffer to be drained
+		buffer.flip();
+		// write to the channel, may block
+		dest.write(buffer);
+		// If partial transfer, shift remainder down
+		// If buffer is empty, same as doing clear()
+		buffer.compact();
+	}
+	// EOF will leave buffer in fill state
+	buffer.flip();
+	// make sure the buffer is fully drained.
+	while (buffer.hasRemaining()) {
+		dest.write(buffer);
+	}
+
+	src.close();
+}
---
+++ modUtils/svySystem.js
@@ -0,0 +1,106 @@
+/*
+ * TODO: Support Mobile Client where applocable in Servoy >7.0
+ */
+
+/**
+ * @properties={typeid:24,uuid:"3F795733-507C-479E-8F8B-986347223050"}
+ */
+function isWebClient() {
+	return application.getApplicationType() == APPLICATION_TYPES.WEB_CLIENT
+}
+
+/**
+ * @properties={typeid:24,uuid:"38954B44-9139-44ED-BD26-E0D56457199D"}
+ */
+function isSmartClient() {
+	return application.getApplicationType() == APPLICATION_TYPES.SMART_CLIENT
+}
+
+/**
+ * @properties={typeid:24,uuid:"109575EA-F6B4-4798-AF4B-E3067E59E166"}
+ */
+function isRuntimeClient() {
+	return application.getApplicationType() == APPLICATION_TYPES.RUNTIME_CLIENT
+}
+
+/**
+ * Returns true if the client is either the Smart or Runtime Client
+ * @properties={typeid:24,uuid:"C68E5945-465D-452D-9A75-CE78E891917F"}
+ */
+function isSwingClient() {
+	return [APPLICATION_TYPES.SMART_CLIENT, APPLICATION_TYPES.RUNTIME_CLIENT].indexOf(application.getApplicationType()) 
+}
+
+//TODO: uncomment in Servoy 7.0
+///**
+// * @properties={typeid:24,uuid:"15818D17-D669-4173-AC74-9F44FD67A168"}
+// */
+//function isMobileClient() {
+//	return application.getApplicationType() == APPLICATION_TYPES.MOBILE_CLIENT
+//}
+
+/**
+ * @properties={typeid:24,uuid:"886FAEB2-6882-4D24-860B-F3804A98986E"}
+ */
+function isWindowsPlatform() {
+	return /Windows/.test(application.getOSName())
+}
+
+/**
+ * @properties={typeid:24,uuid:"3856C8FE-CEA6-4DD3-99F5-02D253DCB5DC"}
+ */
+function isOSXPlatform() {
+	return /Mac/.test(application.getOSName())
+}
+
+/**
+ * @properties={typeid:24,uuid:"BB1D7D24-32C6-4CFF-BAF9-0E682FAA751C"}
+ */
+function isLinuxPlatform() {
+	return /FreeBSD|Linux/.test(application.getOSName())
+	
+}
+
+/**
+ * Tests if the User Agent indicates an iPhone, iPad or iPod device (in Servoy Web Client)
+ * @properties={typeid:24,uuid:"1CADF998-D886-4DE1-B43D-FAB7A7A14AFB"}
+ */
+function isIOSPlatform() {
+	if([APPLICATION_TYPES.WEB_CLIENT].indexOf(application.getApplicationType()) == -1) {
+		return false
+	}
+	/** @type {Packages.org.apache.wicket.protocol.http.request.WebClientInfo} */
+	var clientInfo = Packages.org.apache.wicket.Session.get().getClientInfo()
+	var userAgent = clientInfo.getUserAgent()
+	return /iPhone|iPad|iPod/.test(userAgent)
+}
+
+/**
+ * Tests if the User Agent indicates an Android device (in Servoy Web Client)
+ * @properties={typeid:24,uuid:"95486154-ADC9-49E7-B9EB-12E1B14347FF"}
+ */
+function isAndroidPlatform() {
+	if([APPLICATION_TYPES.WEB_CLIENT].indexOf(application.getApplicationType()) == -1) {
+		return false
+	}
+	
+	/** @type {Packages.org.apache.wicket.protocol.http.request.WebClientInfo} */
+	var clientInfo = Packages.org.apache.wicket.Session.get().getClientInfo()
+	var userAgent = clientInfo.getUserAgent()
+	return /Android/.test(userAgent)
+}
+
+/**
+ * Tests if the User Agent indicates an Android or iOS device (in Servoy Web Client)
+ * @properties={typeid:24,uuid:"B3F3DDE2-4582-4E01-A28D-97DD9AE6CEA8"}
+ */
+function isMobilePlatform() {
+	if([APPLICATION_TYPES.WEB_CLIENT].indexOf(application.getApplicationType()) == -1) {
+		return false
+	}
+	
+	/** @type {Packages.org.apache.wicket.protocol.http.request.WebClientInfo} */
+	var clientInfo = Packages.org.apache.wicket.Session.get().getClientInfo()
+	var userAgent = clientInfo.getUserAgent()
+	return /iPhone|iPad|Android/.test(userAgent)
+}
