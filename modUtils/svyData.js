cab3c2392a8223fcf10545fea19676c87cd7bb84
Paul Bakker <pbakker@servoy.com>
2012-12-07 15:39:38 +0000

    Created a new scope for data related util method, called svyData Deprecated another method in svy_utilities#globals.js, replaced with entry in svyData

---
+++ modUtils/svyData.js
@@ -0,0 +1,23 @@
+/**
+ * Pivots a JSDataSet: First column in the returned JSDataSet will contain the column names of the original dataset.<br>
+ * For each row in the original dataset an column will be added to the returned JSDataSet
+ * 
+ * @param {JSDataSet} dataset The dataset for which to create a pivoted datatset
+ * @return {JSDataSet} a new JSDataSet containing a pivoted copy of the original JSDataSet
+ *
+ * @properties={typeid:24,uuid:"D532BF51-A347-4DF2-844C-B06CAEA70919"}
+ */
+function pivotJSDataSet(dataset) {
+	//Generate column names for the pivoted dataset
+	var columnNames = ['columnName']
+	for (var i = 1; i <= dataset.getMaxRowIndex(); i++) {
+		columnNames.push('row'+i)
+	}
+	var newDS = databaseManager.createEmptyDataSet(0, columnNames)
+	for (i = 1; i <= dataset.getMaxColumnIndex(); i++) {
+		var row = dataset.getColumnAsArray(i)
+		row.splice(0, 0, dataset.getColumnName(i))
+		newDS.addRow(row)
+	}
+	return newDS
+}
---
+++ modUtils_test/svyData_test.js
@@ -0,0 +1,15 @@
+/**
+ * @properties={typeid:24,uuid:"424DD9B5-8D9C-486B-BDA9-2D38E5FB1956"}
+ */
+function testPivotJSDataSet() {
+	var ds = databaseManager.createEmptyDataSet(0,['col1', 'col2'])
+	ds.addRow(['col1_row1','col2_row1'])
+	ds.addRow(['col1_row2','col2_row2'])
+	application.output(ds)
+	
+	var pivoted = scopes.svyData.pivotJSDataSet(ds)
+	jsunit.assertEquals(3,pivoted.getMaxColumnIndex())
+	jsunit.assertEquals(2,pivoted.getMaxRowIndex())
+	jsunit.assertEquals('row2',pivoted.getColumnName(3))
+	jsunit.assertEquals('col2',pivoted.getValue(2,1))
+}
