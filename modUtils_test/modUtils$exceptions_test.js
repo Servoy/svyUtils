804aed66c9dbaff8d425826346a9ee83fcaede88
Sean Devlin <sdevlin@servoy.com>
2013-03-05 02:24:53 +0000

    Refactor of exception usages

---
+++ modUtils_test/modUtils$exceptions_test.js
@@ -0,0 +1,15 @@
+/**
+ * @properties={typeid:24,uuid:"9A19D2B3-0124-4C45-9A13-F8ADC7982B70"}
+ */
+function testExceptionHierarchy() {
+	//for the used technique here, see: http://stackoverflow.com/questions/4152931/javascript-inheritance-call-super-constructor-or-use-prototype-chain
+	var e = new scopes.modUtils$exceptions.IllegalArgumentException('test');
+	jsunit.assertTrue(e instanceof scopes.modUtils$exceptions.SvyException);
+	jsunit.assertEquals(e.getMessage(),'test'); // TODO: This warning should be fixed in Svy 7.0
+	e = new scopes.modUtils$exceptions.IllegalStateException('test');
+	jsunit.assertTrue(e instanceof scopes.modUtils$exceptions.SvyException);
+	jsunit.assertEquals(e.getMessage(),'test'); // TODO: This warning should be fixed in Svy 7.0
+	e = new scopes.modUtils$exceptions.UnsupportedOperationException('test');
+	jsunit.assertTrue(e instanceof scopes.modUtils$exceptions.SvyException);
+	jsunit.assertEquals(e.getMessage(),'test'); // TODO: This warning should be fixed in Svy 7.0
+}
