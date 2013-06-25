b9cea8c17c4751fbf7328e59acf23adc654dd039
Paul Bakker <pbakker@servoy.com>
2013-06-25 20:12:01 +0000

    Added GPL/LGPL license text, added License message at top of first file

--- modUtils/modUtils$IO.js
+++ modUtils/modUtils$IO.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Servoy Business Application Platform, Copyright (C) 2012-2013 Servoy BV 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * <pre>Opens a file from the file system using the default viewer for the fileType on the current platform. (.txt with editor, .pdf with pdf reader, .doc with word, etc.)
  * 
