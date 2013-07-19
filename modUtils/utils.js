55c81b0979863897d37072768558cd3fa38802d0
Paul Bakker <pbakker@servoy.com>
2013-07-19 14:17:44 +0000

    Added utils scope with schortcuts to modUtils$xxxx scopes for convenience

---
+++ modUtils/utils.js
@@ -0,0 +1,44 @@
+/**
+ * @properties={typeid:35,uuid:"42514F5F-F934-4CDE-9A3C-DBDA4FD571AF",variableType:-4}
+ */
+var IO = scopes.modUtils$IO
+
+/**
+ * @properties={typeid:35,uuid:"3AA9C536-1D73-4FD0-82F4-D653CF54648C",variableType:-4}
+ */
+var UI = scopes.modUtils$UI
+
+/**
+ * @properties={typeid:35,uuid:"C9735DD3-B95A-48D8-9C5B-7B80C0E0CA3E",variableType:-4}
+ */
+var data = scopes.modUtils$data
+
+/**
+ * @properties={typeid:35,uuid:"13581C74-8F11-49CA-9727-AAFC4A80B574",variableType:-4}
+ */
+var datasetGrid = scopes.modUtils$datasetGrid
+
+/**
+ * @properties={typeid:35,uuid:"59B75D47-8C66-41A8-9D15-77F88CC81029",variableType:-4}
+ */
+var date = scopes.modUtils$date
+
+/**
+ * @properties={typeid:35,uuid:"143134EB-4932-40F6-8EB8-B01023F2F577",variableType:-4}
+ */
+var eventManager = scopes.modUtils$eventManager
+
+/**
+ * @properties={typeid:35,uuid:"39C5505E-0C34-4CC1-A3B1-A332F40ECCFA",variableType:-4}
+ */
+var exceptions = scopes.modUtils$exceptions
+
+/**
+ * @properties={typeid:35,uuid:"2AAA3C45-B1C9-4F64-8092-05BBE14022C1",variableType:-4}
+ */
+var net = scopes.modUtils$net
+
+/**
+ * @properties={typeid:35,uuid:"E18CFE94-4718-41EA-B4A4-9C7F441B6EC4",variableType:-4}
+ */
+var system = scopes.modUtils$system
