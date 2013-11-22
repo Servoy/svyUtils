24d4db9942a651feda612b43aef580fb802a84a8
Paul Bakker <pbakker@servoy.com>
2013-11-22 07:36:27 +0000

    Phased out usage of scopes.utils. as it currently leads to warnings that cannot be fixed, see  SVY-5419

--- modUtils/utils.js
+++ modUtils/utils.js
@@ -16,46 +16,55 @@
   */
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"42514F5F-F934-4CDE-9A3C-DBDA4FD571AF",variableType:-4}
  */
 var IO = scopes.modUtils$IO
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"3AA9C536-1D73-4FD0-82F4-D653CF54648C",variableType:-4}
  */
 var UI = scopes.modUtils$UI
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"C9735DD3-B95A-48D8-9C5B-7B80C0E0CA3E",variableType:-4}
  */
 var data = scopes.modUtils$data
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"59B75D47-8C66-41A8-9D15-77F88CC81029",variableType:-4}
  */
 var date = scopes.modUtils$date
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"143134EB-4932-40F6-8EB8-B01023F2F577",variableType:-4}
  */
 var eventManager = scopes.modUtils$eventManager
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"39C5505E-0C34-4CC1-A3B1-A332F40ECCFA",variableType:-4}
  */
 var exceptions = scopes.modUtils$exceptions
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"2AAA3C45-B1C9-4F64-8092-05BBE14022C1",variableType:-4}
  */
 var net = scopes.modUtils$net
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"E18CFE94-4718-41EA-B4A4-9C7F441B6EC4",variableType:-4}
  */
 var system = scopes.modUtils$system
 
 /**
+ * @deprecated
  * @properties={typeid:35,uuid:"40EE4F64-6875-48BD-AC4F-DECFAC279295",variableType:-4}
  */
 var log = scopes.modUtils$log
