04a51537edf99e3a27975d764daeb284c9a9d98b
Paul Bakker <pbakker@servoy.com>
2013-12-18 16:09:53 +0000

    Made setFieldHtmlType work in any window/dialog Made browser-side init code for setFieldHtmlType only execute once

--- svyUtils$webClient/svyWebClientUtils.js
+++ svyUtils$webClient/svyWebClientUtils.js
@@ -129,41 +129,8 @@ function setFieldHtmlType(element, type) {
  */
 var initSetFieldHtmlType = function(){
 	if (!scopes.svySystem.isWebClient()) return;
-	//FIXME: this code gets called/added now everytime
 	//TODO: Remove this code after upgrading to newer Wicket version (this "patch is for wicket 1.4))
-	//Override Wicket function to support all HTML5 input types
-	addOnDOMReadyScript("Wicket.Form.serializeInput = function(input) {\
-		var type = input.type.toLowerCase();\
-		switch (type) {\
-			case 'checkbox':\
-			case 'radio':\
-				if (!input.checked) {\
-					return '';\
-				};\
-				/*Intentional fallthrough here*/\
-			case 'text':\
-			case 'password':\
-			case 'hidden':\
-			case 'textarea':\
-			case 'search':\
-			case 'email':\
-			case 'url':\
-			case 'tel':\
-			case 'number':\
-			case 'range':\
-			case 'date':\
-			case 'month':\
-			case 'week':\
-			case 'time':\
-			case 'datetime':\
-			case 'datetime-local':\
-			case 'color':\
-				return Wicket.Form.encode(input.name) + '=' + Wicket.Form.encode(input.value) + '&';\
-				break;\
-			default:\
-				return '';\
-		}\
-	}")
+	addJavaScriptDependancy('media://svyWebClientUtils/svyUtils$wicket.js')
 	
 	setFieldHtmlType.TYPES = {
 		TEXT: 'text',
@@ -1038,8 +1005,7 @@ function updateUI(milliseconds) {
 	checkOperationSupported()
 	if (scopes.svySystem.isWebClient()) {
       c = new Continuation();
-      //FIXME: convert to not use WebClientUtils plugin
-      executeClientsideScript(plugins.WebClientUtils.generateCallbackScript(updateUIResume));
+      executeClientsideScript(getCallbackScript(updateUIResume));
       terminator();
    } else {
       application.updateUI(milliseconds)
