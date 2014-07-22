3fd76945d7df1bb484ff77d666504d14a17a5ab3
paronne <paronne@servoy.com>
2014-07-22 17:46:59 +0200

    onOpenMethod to initiate the scope istanbul_scope

--- svyUtils_test/svyUtils_test.js
+++ svyUtils_test/svyUtils_test.js
@@ -33,3 +33,18 @@ function onSolutionClose(force) {
 	} 
 	return true
 }
+
+/**
+ * Callback method for when solution is opened.
+ * When deeplinking into solutions, the argument part of the deeplink url will be passed in as the first argument
+ * All query parameters + the argument of the deeplink url will be passed in as the second argument
+ * For more information on deeplinking, see the chapters on the different Clients in the Deployment Guide.
+ *
+ * @param {String} arg startup argument part of the deeplink url with which the Client was started
+ * @param {Object<Array<String>>} queryParams all query parameters of the deeplink url with which the Client was started
+ *
+ * @properties={typeid:24,uuid:"4E14F1C2-C4DB-46FF-A83B-A340D75BB1E7"}
+ */
+function onSolutionOpen(arg, queryParams) {
+	scopes.istanbul_scope;
+}
