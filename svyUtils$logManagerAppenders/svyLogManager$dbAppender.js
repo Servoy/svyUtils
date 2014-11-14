94a11bfcd4f7fbd46004f64468c1268a4c2cd11b
Patrick Ruhsert <pruhsert@servoy.com>
2014-11-14 12:59:11 +0100

    Added a DB and Rolling File Appender in module svyUtils$logManagerAppenders

---
+++ svyUtils$logManagerAppenders/svyLogManager$dbAppender.js
@@ -0,0 +1,223 @@
+/*
+ * This file is part of the Servoy Business Application Platform, Copyright (C) 2012-2013 Servoy BV 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Example configuration for logging to a database table. The configuration below
+ * will write all logging output of any logger to the console and the DB
+ * 
+ * Please make sure the datasource and dbMapping match your table
+ * 
+ * scopes.svyLogManager.loadConfig({
+ * 		status: "error",
+ * 		plugins: 'scopes.svyLogManager$dbAppender.DbAppender',
+ * 		appenders: [{
+ * 			type: "ApplicationOutputAppender",
+ * 			name: "ApplicationOutput",
+ * 			PatternLayout: {
+ * 				pattern: "%5level %logger{1.} - %msg"
+ * 			}
+ * 		}, {
+ * 			type: "scopes.svyLogManager$dbAppender.DbAppender",
+ * 			name: "DbAppender",
+ * 			datasource: "db:/my_db/log_events",
+ * 			userId: globals.myUserId,
+ * 			dbMapping: { eventTimeColumnName: "event_time", loggerColumnName: "logger_name", logLevelColumnName: "log_level", logMessageColumnName: "log_message", userIdColumName: "user_id", solutionColumnName: "solution_name" },
+ * 			PatternLayout: {
+ * 				pattern: ""
+ * 			}
+ * 		}],
+ * 		loggers: {
+ * 			root: {
+ * 				level: "warn",
+ * 				AppenderRef: [{ref: "DbAppender"}, {ref: "ApplicationOutput"}]
+ * 			}
+ * 		}
+ * 	});
+ * 
+ */
+
+/**
+ * Type definition for the table mapping used to match the logging events to the table's columns
+ * 
+ * @typedef {{
+ * 	eventTimeColumnName: String, 
+ * 	loggerColumnName: String=, 
+ * 	logLevelColumnName: String=, 
+ * 	logMessageColumnName: String, 
+ * 	userIdColumName: String=,
+ * 	solutionColumnName: String=
+ * }}
+ *
+ * @properties={typeid:35,uuid:"EBB28330-3735-4D2C-8364-11203E69E914",variableType:-4}
+ */
+var DB_MAPPING;
+
+/**
+ * Simple Appender that writes log output to the given datasource using the given mapping<br>
+ * <br>
+ * Logs the Level.ALL and Level.TRACE as Level.DEBUG<br>
+ * <br>
+ * When using this appender it needs to be added to the list of plugins in the config as
+ * <pre>scopes.svyLogManager$dbAppender.DbAppender</pre>
+ * see <code>scopes.svyLogManager.CONFIG_TYPE_DEF</code> for details<br>
+ * 
+ * @constructor
+ * @extends {scopes.svyLogManager.AbstractAppender}
+ *
+ * @param {String} datasource
+ * @param {DB_MAPPING} dbMapping
+ * @param {String} userId
+ *
+ * @properties={typeid:24,uuid:"6EDA8242-99D0-46B8-AFD4-F27405C35DD9"}
+ */
+function DbAppender(datasource, dbMapping, userId) {
+	
+	scopes.svyLogManager.AbstractAppender.call(this);
+
+	/**
+	 * the datasource for this appender
+	 */
+	this.datasource = datasource;
+
+	/**
+	 * the mapping used
+	 */
+	this.dbMapping = dbMapping;
+	
+	/**
+	 * a foundset for the datasource
+	 */
+	this.foundset = null;
+	
+	/**
+	 * The userId
+	 */
+	this.userId = userId ? userId : null;
+	
+	try {
+		/**
+		 * a foundset for the datasource
+		 */
+		this.foundset = databaseManager.getFoundSet(this.datasource);
+	} catch(e) {
+		scopes.svyLogManager.getStatusLogger().error("DbAppender couldn't find datasource \"" + this.datasource + "\"", e);
+		throw e;
+	}
+	var jsTable = databaseManager.getTable(this.datasource);
+	var columnNames = jsTable.getColumnNames();
+	for ( var i in this.dbMapping ) {
+		if (columnNames.indexOf(this.dbMapping[i]) == -1) {
+			scopes.svyLogManager.getStatusLogger().error("DbAppender couldn't find given column \"" + this.dbMapping[i] + "\" in datasource \"" + this.datasource + "\"");
+		}
+	}
+}
+
+/**
+ * @private
+ * @SuppressWarnings(unused)
+ * @properties={typeid:35,uuid:"18BB30C5-C6E5-4E2C-A505-1231344DBCFB",variableType:-4}
+ */
+var initDbAppender = (function() {
+	DbAppender.prototype = Object.create(scopes.svyLogManager.AbstractAppender.prototype);
+	DbAppender.prototype.constructor = DbAppender
+
+	/**
+	 * @this {DbAppender}
+	 * @param {scopes.svyLogManager.LoggingEvent} loggingEvent
+	 */
+	DbAppender.prototype.append = function(loggingEvent) {
+		var lvl = loggingEvent.level.intLevel
+		if (lvl < LOGGINGLEVEL.DEBUG) { // All or TRACE
+			lvl = LOGGINGLEVEL.DEBUG
+		} else if (lvl > LOGGINGLEVEL.FATAL) { //OFF
+			return
+		}
+		
+		var record = this.foundset.getRecord(this.foundset.newRecord());
+
+		if (this.dbMapping.eventTimeColumnName) record[this.dbMapping.eventTimeColumnName] = loggingEvent.timeStamp;
+		if (this.dbMapping.logLevelColumnName) record[this.dbMapping.logLevelColumnName] = loggingEvent.level.name;
+		if (this.dbMapping.logMessageColumnName) record[this.dbMapping.logMessageColumnName] = loggingEvent.message.getFormattedMessage();
+		if (this.dbMapping.userIdColumName && this.userId) record[this.dbMapping.userIdColumName] = this.userId;
+		if (this.dbMapping.loggerColumnName) record[this.dbMapping.loggerColumnName] = loggingEvent.logger.name;
+		if (this.dbMapping.solutionColumnName) record[this.dbMapping.solutionColumnName] = application.getSolutionName();
+
+		var success = databaseManager.saveData(record);
+		if (!success) {
+			scopes.svyLogManager.getStatusLogger().error("DbAppender failed to save log message to datasource \"" + this.datasource + "\"");
+		}
+	}
+
+	DbAppender.prototype.toString = function() {
+		return 'DbAppender'
+	}
+
+	DbAppender.PluginFactory = {
+		parameters: [{
+			configName: 'name',
+			type: 'string'
+		}, {
+			configName: 'Layout',
+			type: scopes.svyLogManager.AbstractLayout
+		}, {
+			configName: 'datasource',
+			type: 'string'
+		}, {
+			configName: 'dbMapping',
+			type: '{ eventTimeColumnName: String, loggerColumnName: String, logLevelColumnName: String,  logMessageColumnName: String, userIdColumName: String,solutionColumnName: String}'
+		}, {
+			configName: 'userId',
+			type: 'string'
+		}],
+
+		/**
+		 * @param {String} name
+		 * @param {scopes.svyLogManager.AbstractLayout} layout
+		 * @param {String} datasource
+		 * @param {DB_MAPPING} dbMapping
+		 * @param {String} userId
+		 * @return {DbAppender}
+		 */
+		create: function(name, layout, datasource, dbMapping, userId) {
+			var retval = new DbAppender(datasource, dbMapping, userId);
+			retval.setName(name);
+			retval.setLayout(layout);
+			return retval;
+		}
+	}
+}());
+
+/**
+ * Returns an object describing the appender that can be used in the logManager config
+ * @public 
+ *
+ * @param {String} datasource the database table where the DbAppender should append to
+ * @param {DB_MAPPING} dbMapping the mapping used to match logging events to the table's columns
+ *
+ * @properties={typeid:24,uuid:"4CF81578-F78E-46E9-8741-34AFCE1E9976"}
+ */
+function getAppenderConfigObject(datasource, dbMapping) {
+	return {
+		type: "scopes.svyLogManager$dbAppender.DbAppender",
+		name: "DbAppender",
+		datasource: datasource,
+		dbMapping: { eventTimeColumnName: dbMapping.eventTimeColumnName, loggerColumnName: dbMapping.logLevelColumnName, logLevelColumnName: dbMapping.logLevelColumnName, logMessageColumnName: dbMapping.logMessageColumnName, userIdColumName: dbMapping.userIdColumName, solutionColumnName: dbMapping.solutionColumnName },
+		PatternLayout: {
+			pattern: "%5level %logger{1.} - %msg"
+		}
+	}
+}
---
+++ svyUtils$logManagerAppenders/svyLogManager$rollingFileAppender.js
@@ -0,0 +1,282 @@
+/*
+ * This file is part of the Servoy Business Application Platform, Copyright (C) 2012-2013 Servoy BV 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Example configuration for logging to a file. The configuration below
+ * will write all logging output of any logger to the console and the log file
+ * 
+ * Please make sure the datasource and dbMapping match your table
+ * 
+ * scopes.svyLogManager.loadConfig({
+ * 		status: "error",
+ * 		plugins: 'scopes.svyLogManager$rollingFileAppender.RollingFileAppender',
+ * 		appenders: [{
+ * 			type: "ApplicationOutputAppender",
+ * 			name: "ApplicationOutput",
+ * 			PatternLayout: {
+ * 				pattern: "%5level %logger{1.} - %msg"
+ * 			}
+ * 		}, {
+ * 			type: "scopes.svyLogManager$rollingFileAppender.RollingFileAppender",
+ * 			name: "DbAppender",
+ * 			fileName: "logs/application.log",
+ * 			maxFileSize: 10 * 1024 * 1024,
+ * 			maxBackupIndex: 5,
+ * 			PatternLayout: {
+ * 				pattern: "%date %5level %30logger - %msg"
+ * 			}
+ * 		}],
+ * 		loggers: {
+ * 			root: {
+ * 				level: "warn",
+ * 				AppenderRef: [{ref: "DbAppender"}, {ref: "ApplicationOutput"}]
+ * 			}
+ * 		}
+ * 	});
+ * 
+ */
+
+/**
+ * Simple Appender that logs to files, optionally keeping a number of backups when the max file size is reached<br>
+ * <br>
+ * Logs the Level.ALL and Level.TRACE as Level.DEBUG<br>
+ * <br>
+ * @constructor
+ * @extends {scopes.svyLogManager.AbstractAppender}
+ *
+ * @param {String} fileName
+ * @param {Number} maxFileSize
+ * @param {Number} maxBackupIndex
+ *
+ * @properties={typeid:24,uuid:"9C2D8753-F61A-4638-8D39-0ECE80CD1CCE"}
+ */
+function RollingFileAppender(fileName, maxFileSize, maxBackupIndex) {
+	scopes.svyLogManager.AbstractAppender.call(this);
+
+	/**
+	 * Log file name
+	 */
+	this.fileName = fileName;
+	
+	/**
+	 * Maximum file size
+	 */
+	this.maxFileSize = maxFileSize ? maxFileSize : 10485760;
+	
+	/**
+	 * Maximum number of backups
+	 */
+	this.maxBackupIndex = maxBackupIndex >= 0 ? maxBackupIndex : 5;
+	
+	/**
+	 * Next rollover
+	 */
+	this.nextRollover = 0;
+	
+	/**
+	 * The current size of the file
+	 */
+	this.fileSize = 0;
+	
+	/**
+	 * @type {java.io.OutputStreamWriter}
+	 */
+	this.writer = null;
+	
+}
+
+/**
+ * @private
+ *
+ * @type {Object}
+ * 
+ * @SuppressWarnings(unused)
+ *
+ * @properties={typeid:35,uuid:"135811BC-7D8F-4233-BF25-54F7314895FC",variableType:-4}
+ */
+var initRollingFileAppender = (function() {
+		RollingFileAppender.prototype = Object.create(scopes.svyLogManager.AbstractAppender.prototype);
+		RollingFileAppender.prototype.constructor = RollingFileAppender;
+
+		/**
+		 * @this {RollingFileAppender}
+		 * @param {String} fileName
+		 * @param {boolean} append
+		 */
+		RollingFileAppender.prototype.setFile = function(fileName, append) {
+			try {
+				var logger = scopes.svyLogManager.getStatusLogger();
+				if (this.writer != null) {
+					this.writer.close();
+				}
+				this.fileName = utils.stringTrim(fileName);
+				var file = plugins.file.convertToJSFile(this.fileName);
+				if (append) {
+					this.fileSize = file.size();
+				} else {
+					logger.debug("No backups set; deleting log file " + file);
+					file.deleteFile();
+					this.fileSize = 0;
+				}
+				var fos = new java.io.FileOutputStream(this.fileName, true);
+				this.writer = new java.io.OutputStreamWriter(fos, "UTF-8");
+				logger.debug("Now using file " + this.fileName);
+			} catch (e) {
+				var parentName = file.getParent();
+				if (parentName != null) {
+					var parentDir = plugins.file.convertToJSFile(parentName);
+					if (!parentDir.exists() && parentDir.mkdirs()) {
+						fos = new java.io.FileOutputStream(this.fileName, append);
+						this.writer = new java.io.OutputStreamWriter(fos, "UTF-8");
+					} else {
+						throw e;
+					}
+				} else {
+					throw e;
+				}
+			}
+		}
+
+		/**
+		 * @this {RollingFileAppender}
+		 */
+		RollingFileAppender.prototype.rollOver = function() {
+			var target;
+			var file;
+
+			var logger = scopes.svyLogManager.getStatusLogger();
+
+			logger.debug("rolling over count=" + this.fileSize);
+			// if operation fails, do not roll again until
+			// maxFileSize more bytes are written
+			this.nextRollover = this.fileSize + this.maxFileSize;
+			logger.debug("maxBackupIndex=" + this.maxBackupIndex);
+
+			var renameSucceeded = true;
+			// If maxBackups <= 0, then there is no file renaming to be done.
+			if (this.maxBackupIndex > 0) {
+				// Delete the oldest file
+				file = plugins.file.convertToJSFile(this.fileName + '.' + this.maxBackupIndex);
+				if (file.exists()) {
+					logger.debug("Deleting oldest backup file " + file);
+					renameSucceeded = file.deleteFile();
+				}
+				// Map {(maxBackupIndex - 1), ..., 2, 1} to {maxBackupIndex, ..., 3, 2}
+				for (var i = this.maxBackupIndex - 1; i >= 1 && renameSucceeded; i--) {
+					file = plugins.file.convertToJSFile(this.fileName + "." + i);
+					if (file.exists()) {
+						target = plugins.file.convertToJSFile(this.fileName + '.' + (i + 1));
+						logger.debug("Renaming file " + file + " to " + target);
+						renameSucceeded = file.renameTo(target);
+					}
+				}
+
+				if (renameSucceeded) {
+					// Rename fileName to fileName.1
+					target = plugins.file.convertToJSFile(this.fileName + "." + 1);
+
+					this.writer.close(); // keep windows happy.
+
+					file = plugins.file.convertToJSFile(this.fileName);
+					logger.debug("Renaming file " + file + " to " + target);
+					renameSucceeded = file.renameTo(target);
+					// if file rename failed, reopen file with append = true
+					if (!renameSucceeded) {
+						try {
+							this.setFile(this.fileName, true);
+						} catch (e) {
+							logger.error("setFile(" + this.fileName + ", true) call failed.", e);
+						}
+					}
+				}
+			}
+
+			// if all renames were successful, then
+			if (renameSucceeded) {
+				try {
+					this.setFile(this.fileName, false);
+					this.nextRollover = 0;
+				} catch (e) {
+					logger.error("setFile(" + this.fileName + ", false) call failed.", e);
+				}
+			}
+		}
+
+		/**
+		 * @this {RollingFileAppender}
+		 * @param {scopes.svyLogManager.LoggingEvent} loggingEvent
+		 */
+		RollingFileAppender.prototype.append = function(loggingEvent) {
+			var lvl = loggingEvent.level.intLevel
+			if (lvl < LOGGINGLEVEL.DEBUG) { //All or TRACE
+				lvl = LOGGINGLEVEL.DEBUG
+			} else if (lvl > LOGGINGLEVEL.FATAL) { //OFF
+				return
+			}
+
+			try {
+				var msgToWrite = this.layout.format(loggingEvent) + "\r\n";
+				this.writer.append(msgToWrite);
+				this.fileSize += msgToWrite.length;
+				this.writer.flush();
+				if (this.fileSize >= this.maxFileSize && this.fileSize >= this.nextRollover) {
+					this.rollOver();
+				}
+			} catch (e) {
+				scopes.svyLogManager.getStatusLogger().error("RollingFileAppender error writing to log file", e);
+			}
+		}
+
+		RollingFileAppender.prototype.toString = function() {
+			return 'RollingFileAppender'
+		}
+
+		RollingFileAppender.PluginFactory = {
+			parameters: [{
+				configName: 'name',
+				type: 'string'
+			}, {
+				configName: 'Layout',
+				type: scopes.svyLogManager.AbstractLayout
+			}, {
+				configName: 'fileName',
+				type: 'string'
+			}, {
+				configName: 'maxFileSize',
+				type: 'int'
+			}, {
+				configName: 'maxBackupIndex',
+				type: 'int'
+			}],
+
+			/**
+			 * @param {String} name
+			 * @param {scopes.svyLogManager.AbstractLayout} layout
+			 * @param {String} fileName
+			 * @param {Number} [maxFileSize]
+			 * @param {Number} [maxBackupIndex]
+			 * @return {RollingFileAppender}
+			 */
+			create: function(name, layout, fileName, maxFileSize, maxBackupIndex) {
+				var retval = new RollingFileAppender(fileName, maxFileSize, maxBackupIndex);
+				retval.setName(name)
+				retval.setLayout(layout)
+				retval.setFile(fileName, true);
+				return retval
+			}
+		}
+}())
