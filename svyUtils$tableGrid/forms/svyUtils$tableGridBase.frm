97c12769873e59615085aedfe51cea5a87a1ec38
Patrick Ruhsert <pruhsert@servoy.com>
2014-12-23 11:21:34 +0100

    Added svyUtils$tableGrid

---
+++ svyUtils$tableGrid/forms/svyUtils$tableGridBase.frm
@@ -0,0 +1,5 @@
+name:"svyUtils$tableGridBase",
+navigatorID:"-1",
+showInMenu:true,
+typeid:3,
+uuid:"6064B04B-E1E3-440F-87E0-B9CA4F91926D"
---
+++ svyUtils$tableGrid/forms/svyUtils$tableGridBase.js
@@ -0,0 +1,8 @@
+/**
+ * The scopes.svyUtils$tableGrid.TableGrid object
+ * 
+ * @type {scopes.svyUtils$tableGrid.TableGrid}
+ *
+ * @properties={typeid:35,uuid:"F212BC4C-3F5F-44D3-92B2-8A2E476E77B1",variableType:-4}
+ */
+var tableGrid = null;
