65e436365c60cde51e2bfccbcab3f25adb3e9440
Paul Bakker <pbakker@gmail.com>
2015-03-09 13:47:13 +0100

    Added missing description on setFormHeight

--- svyUtils$smartClient/svySmartClientUtils.js
+++ svyUtils$smartClient/svySmartClientUtils.js
@@ -92,7 +92,12 @@ function getFormName(element) {
 }
 
 /**
- * Forces 
+ * Allow runtime updating the height of the body part of a form in RECORD_VIEW<br/> 
+ * <br/>
+ * This allows to mimic the behavior of the Web Client when placing elements at runtime using element.setLocation() below the bounds of the designtime form<br/> 
+ * In the Web Client this will automatically cause scrollbars to appear, but not so in the Smart Client<br/>
+ * </br>
+ * This method allows the height of the BODY part to be set at Runtime, without having to go through the SolutionModel and having to do a controller.recreateUI()
  * 
  * @param {RuntimeForm} form
  * @param {Number} height
