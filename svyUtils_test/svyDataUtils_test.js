37bb61b6507c0156039878732567d749da01407d
paronne <paronne@servoy.com>
2017-09-08 11:05:53 +0200

    svyDataUtils.selectRecord and selectRecordByPks

--- svyUtils_test/svyDataUtils_test.js
+++ svyUtils_test/svyDataUtils_test.js
@@ -1,4 +1,5 @@
 /**
+ * @protected 
  * @properties={typeid:24,uuid:"18B1414B-23A8-4DFB-BCCE-3AE8A6BB6F2D"}
  */
 function testPivotJSDataSet() {
@@ -11,4 +12,62 @@ function testPivotJSDataSet() {
 	jsunit.assertEquals(2,pivoted.getMaxRowIndex())
 	jsunit.assertEquals('row2',pivoted.getColumnName(3))
 	jsunit.assertEquals('col2',pivoted.getValue(2,1))
+}
+
+/**
+ * @protected 
+ * @properties={typeid:24,uuid:"B7515A10-28AF-4D2C-9B4A-D800769A5584"}
+ */
+function testSelectRecord() {
+	var orderDetails = datasources.db.example_data.order_details.getFoundSet();
+	orderDetails.loadAllRecords();
+	var orderDetailsCopy = orderDetails.duplicateFoundSet();
+	// get the last record
+	var count = databaseManager.getFoundSetCount(orderDetailsCopy);
+	if (count > 403) {
+		var order = orderDetailsCopy.getRecord(count - 202);
+		
+		// select the record at pos count - 202
+		jsunit.assertTrue('selecteRecord should select record ' + order.orderid + ',' + order.productid, scopes.svyDataUtils.selectRecord(orderDetails, order));
+		jsunit.assertEquals('selectRecord should select the correct record '  + order.orderid, order.orderid, orderDetails.orderid);
+		jsunit.assertEquals('selectRecord should select the correct record '  + order.productid, order.productid, orderDetails.productid);
+
+	}
+	
+	var lastOrder = orderDetailsCopy.getRecord(count);
+	
+	// select the record at pos count - 202
+	jsunit.assertTrue('selecteRecord should select record ' + lastOrder.orderid + ',' + lastOrder.productid, scopes.svyDataUtils.selectRecord(orderDetails, lastOrder));
+	jsunit.assertEquals('selectRecord should select the correct record '  + lastOrder.orderid, lastOrder.orderid, orderDetails.orderid);
+	jsunit.assertEquals('selectRecord should select the correct record '  + lastOrder.productid, lastOrder.productid, orderDetails.productid);
+	
+}
+
+/**
+ * @protected 
+ * @properties={typeid:24,uuid:"DD9D17AD-AF8E-4B62-B28B-D65F7C65EB97"}
+ */
+function testSelectRecordByPks() {
+	var orderDetails = datasources.db.example_data.order_details.getFoundSet();
+	orderDetails.loadAllRecords();
+	var orderDetailsCopy = orderDetails.duplicateFoundSet();
+	// get the last record
+	var count = databaseManager.getFoundSetCount(orderDetailsCopy);
+	if (count > 403) {
+		var order = orderDetailsCopy.getRecord(count - 202);
+		
+		// select the record at pos count - 202
+		jsunit.assertTrue('selecteRecord should select record ' + order.orderid + ',' + order.productid, scopes.svyDataUtils.selectRecordByPks(orderDetails, order.orderid, order.productid));
+		jsunit.assertEquals('selectRecord should select the correct record '  + order.orderid, order.orderid, orderDetails.orderid);
+		jsunit.assertEquals('selectRecord should select the correct record '  + order.productid, order.productid, orderDetails.productid);
+
+	}
+	
+	var lastOrder = orderDetailsCopy.getRecord(count);
+	
+	// select the record at pos count - 202
+	jsunit.assertTrue('selecteRecord should select record ' + lastOrder.orderid + ',' + lastOrder.productid, scopes.svyDataUtils.selectRecordByPks(orderDetails, lastOrder.orderid, lastOrder.productid));
+	jsunit.assertEquals('selectRecord should select the correct record '  + lastOrder.orderid, lastOrder.orderid, orderDetails.orderid);
+	jsunit.assertEquals('selectRecord should select the correct record '  + lastOrder.productid, lastOrder.productid, orderDetails.productid);
+	
 }
