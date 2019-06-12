0bc3670cfacaa0f7967f4031d4bb314b2f6df3f3
Marc Boegem <marc@solutiative.nl>
2019-06-13 01:16:52 +0200

    fix wrong enumerable for getting DoY

--- svyUtils/svyDateUtils.js
+++ svyUtils/svyDateUtils.js
@@ -967,7 +967,7 @@ function getDayOfWeek(date, useISO8601) {
  */
 function getDayOfYear(date) {
 	calendar.setTimeInMillis(date.getTime());
-	return calendar.get(java.util.Calendar.DAY_OF_WEEK);
+	return calendar.get(java.util.Calendar.DAY_OF_YEAR);
 }
 
 /**
